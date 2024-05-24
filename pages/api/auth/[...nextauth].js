ab13b8e195c7169733c32fd2e109507fc65f1781
Souvik Sarkar <souviksarkar2k3@gmail.com>
2024-05-24 17:19:10 +0530

    JWT token is generated while authentication

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,21 +1,31 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 import GitHubProvider from "next-auth/providers/github"
-import DiscordProvider from "next-auth/providers/discord"
 
 export default NextAuth({
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_ID,
-            clientSecret: process.env.GOOGLE_SECRET
-        }),
-        GitHubProvider({
-            clientId: process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET
-        }),
-        DiscordProvider({
-            clientId: process.env.DISCORD_ID,
-            clientSecret: process.env.DISCORD_SECRET
-        })
-    ]
+	providers: [
+		GoogleProvider({
+			clientId: process.env.GOOGLE_ID,
+			clientSecret: process.env.GOOGLE_SECRET
+		}),
+		GitHubProvider({
+			clientId: process.env.GITHUB_ID,
+			clientSecret: process.env.GITHUB_SECRET
+		})
+	],
+	callbacks: {
+		async jwt({ token, user, account }) {
+			if (account && user) {
+				token.accessToken = account.access_token
+			}
+			return token
+		},
+		async session({ session, token }) {
+			session.accessToken = token.accessToken
+			return session
+		}
+	},
+	session: {
+		strategy: 'jwt'
+	}
 })
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,11 @@
 import { useSession, signIn, signOut } from "next-auth/react"
-import { io } from "socket.io-client"
 
 export default function Home() {
-  const { data: session } = useSession()
-  const socket = io("http://localhost:5000")
-  socket.on("connect", () => console.log('Client connected', socket.id))
+  const session = useSession()
+  console.log(session)
   return (<main className="flex min-h-screen flex-col items-center justify-between p-24">
     <section className="cursor-pointer">
-      {session
+      {session.data
         ? <div onClick={() => signOut()}>Sign Out</div>
         : <div onClick={() => signIn()}>Sign In</div>
       }
